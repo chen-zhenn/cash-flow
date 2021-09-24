1466110c0ece95bfdd0cb28b770cfaed0b71fa9d
carlos <carlos@visie.com.br>
2021-09-24 13:06:53 -0300

    Ocultando para git alguns diretórios de build em dist e deletando os mesmos...

--- front-end/dist/js/view/View.js
+++
@@ -1,9 +0,0 @@
-class View {
-    constructor(element) {
-        this._element = document.querySelector(element);
-    }
-    update(data) {
-        this._element.innerHTML = this.template(data);
-    }
-}
-export { View };
