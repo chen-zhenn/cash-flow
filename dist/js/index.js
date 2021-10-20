55e9f9cb025e5c46fa6140235ba922dcc5b31368
carlos <carlos@visie.com.br>
2021-10-20 03:51:40 -0300

    Removendo diretórios e arquivos fora da estrutura padrão...

--- dist/js/index.js
+++
@@ -1,4 +0,0 @@
-import { ExpenseController } from "./controller/ExpenseController.js";
-const expense = new ExpenseController();
-const $btnSubmit = document.getElementById('btn-dp-submit');
-$btnSubmit.addEventListener('click', event => expense.add());
--- dist/js/model/ExpenseModel.js
+++
@@ -1,8 +0,0 @@
-class ExpenseModel {
-    constructor(category, description, currency) {
-        this._category = category;
-        this._description = description;
-        this._currency = currency;
-    }
-}
-export { ExpenseModel };
